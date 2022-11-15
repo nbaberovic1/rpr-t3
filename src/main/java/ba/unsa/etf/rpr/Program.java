d05fa61bb0ace2b2e482fd85a4ed47e9b6e43be8
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2022-11-15 21:29:45 +0100

    Podešavanje mavena

--- src/main/java/ba/unsa/etf/rpr/Program.java
+++ src/main/java/ba/unsa/etf/rpr/Program.java
@@ -1,11 +1,7 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+
+public class Program {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
