91e77e491e08eb3f4f686d01d0ecdc8b1fd9d53b
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2022-11-16 22:37:13 +0100

    Implementacija klase MedunarodniBroj

--- src/main/java/ba/unsa/etf/rpr/MedunarodniBroj.java
+++ src/main/java/ba/unsa/etf/rpr/MedunarodniBroj.java
@@ -1,4 +1,49 @@
 package ba.unsa.etf.rpr;
 
-public class MedunarodniBroj {
+import java.util.Objects;
+
+/**
+ * klasa koja predstavlja medunarodni broj
+ */
+public class MedunarodniBroj extends TelefonskiBroj{
+
+    private String medunarodniBroj; //cuva string medunarodnog broja
+
+    /**
+     * konstrukor klase MedunarodniBroj
+     * @param drzava kompletan pozivni broj drzave
+     * @param broj
+     */
+    public MedunarodniBroj(String drzava, String broj){
+        this.medunarodniBroj = new StringBuilder().append("(").append(drzava).append(")").append(broj).toString();
+    }
+
+    /**
+     * vraca string medunarodnog broja spreman za ispis
+     * @return string medunarodnog broja
+     */
+    public String ispisi() {
+        return medunarodniBroj;
+    }
+
+    /**
+     * generisana equals metoda
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MedunarodniBroj that = (MedunarodniBroj) o;
+        return Objects.equals(medunarodniBroj, that.medunarodniBroj);
+    }
+
+    /**
+     * generisana hashCode metoda
+     * @return
+     */
+    public int hashCode() {
+        return Objects.hash(medunarodniBroj);
+    }
 }
