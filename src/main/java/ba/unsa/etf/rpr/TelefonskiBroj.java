b4b6359aec869e49cbfb0895f88d8ad64843bcb4
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2022-11-16 12:30:30 +0100

    Klasa TelefonskiBroj

---
+++ src/main/java/ba/unsa/etf/rpr/TelefonskiBroj.java
@@ -0,0 +1,6 @@
+package ba.unsa.etf.rpr;
+
+abstract class TelefonskiBroj {
+    public abstract String ispisi(); //vraća broj u tekstualnoj formi pogodnoj za ispis
+    public abstract int hashCode(); //vraća hash code
+}
